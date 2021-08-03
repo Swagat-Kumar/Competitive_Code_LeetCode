60439d4774b663bbbcd7302b2e700b18322608a1
Swagat Kumar <swagat.kumar.code@gmail.com>
2021-08-03 13:04:24 +0530

    Time: 64 ms (19.69%), Space: 14.4 MB (68.90%) - LeetHub

--- interleaving-string/interleaving-string.py
+++ interleaving-string/interleaving-string.py
@@ -2,15 +2,18 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1)+len(s2)!=len(s3):
             return False
-        dp=[False]*(len(s2)+1)
-        for i in range(len(s1)+1):
-            for j in range(len(s2)+1):
-                if i==0 and j==0:
-                    dp[j]=True
-                elif i==0:
-                    dp[j]=dp[j-1] and s2[j-1]==s3[i+j-1]
-                elif j==0:
-                    dp[j]=dp[j] and s1[i-1]==s3[i+j-1]
+        dp=[[False]*(len(s2)+1) for i in range(len(s1)+1)]
+        for r in range(len(s1)+1):
+            for c in range(len(s2)+1):
+                if r==0 and c==0:
+                    dp[r][c]=True
+                elif r==0:
+                    if s2[c-1]==s3[c-1]:
+                        dp[r][c]=dp[r][c-1]
+                elif c==0:
+                    if s1[r-1]==s3[r-1]:
+                        dp[r][c]=dp[r-1][c]
                 else:
-                    dp[j]=(dp[j] and s1[i-1]==s3[i+j-1])or(dp[j-1] and s2[j-1]==s3[i+j-1])
-        return dp[-1]
+                    dp[r][c]=s3[r+c-1]==s2[c-1] and dp[r][c-1]
+                    dp[r][c]=dp[r][c] or (s3[r+c-1]==s1[r-1] and dp[r-1][c])
+        return dp[-1][-1]
